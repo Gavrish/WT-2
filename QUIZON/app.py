bbfa0eabe4add9b48b8c83710fe3f55536d54d04
Gavrish Prabhu <gavrish14@gmail.com>
2018-11-23 02:08:04 +0530

    Quiz Dashboard changes

--- QUIZON/app.py
+++ QUIZON/app.py
@@ -1,19 +1,43 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 import db_utils
+import numpy
+from collections import defaultdict
 import os
 
 app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/login.html')
+@app.route('/login.html', methods = ['GET', 'POST'])
 def login():
 	return render_template('login.html')
 
 
 @app.route('/home_page.html')
 def home_page():
-    return render_template('home_page.html')
+	username = 'g'
+	qid = db.execute_query_string("select qid ,result from user_quiz where username = '"+ username + "'")
+	print(qid)
+	quiz=[]
+
+	for i in qid:
+		d1 =defaultdict()
+		id = str( i['qid'])
+		qname = db.execute_query_string("select qname from quiz where qid = '"+ id + "'")
+		print(qname)
+		d1['qid'] = id
+		d1['result'] = i['result']
+		d1['qname'] = qname[0]['qname']
+		quiz.append(d1)
+		print(quiz)
+
+	res =[]
+	for i in quiz:
+		res.append(i['result'])
+	
+	over = numpy.mean(res)
+	print(over)
+	return render_template('home_page.html', quiz = quiz, over = over)
 
 
 @app.route('/take_quiz.html')
--- QUIZON/db/postgres_init.sql
+++ QUIZON/db/postgres_init.sql
@@ -5,11 +5,17 @@ drop database quizon;
 create database quizon;
 \c quizon
 
-create table users(username varchar(30) primary key, email varchar(30));
+create table users(id serial, username varchar(30) primary key, overall float);
 
 create table quiz(qid serial primary key, qname varchar(15));  
 
-create table user_quiz(uqid serial primary key, qid int, username varchar(30), result int, FOREIGN KEY(username) REFERENCES users(username), FOREIGN KEY(qid) REFERENCES quiz(qid));
+create table user_quiz(uqid serial primary key, qid int, username varchar(30), result float, FOREIGN KEY(username) REFERENCES users(username), FOREIGN KEY(qid) REFERENCES quiz(qid));
+
+insert into users (username,overall) values ('g',0);
+insert into quiz (qname) values ('celebrity');
+insert into quiz (qname) values ('G.K');
+insert into user_quiz (qid,username,result) values (1,'g',5);
+insert into user_quiz (qid,username,result) values (2,'g',4);
 
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO root;
 GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO root;
