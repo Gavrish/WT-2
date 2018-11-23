b3329c82552e0711022c3ff2244b343054cac4de
Gavrish Prabhu <gavrish14@gmail.com>
2018-11-23 23:15:34 +0530

    Dummy Intelligence

--- QUIZON/db/postgres_init.sql
+++ QUIZON/db/postgres_init.sql
@@ -11,8 +11,9 @@ create table quiz(qid serial primary key, qname varchar(15));
 
 create table user_quiz(uqid serial primary key, qid int, username varchar(30), result float, FOREIGN KEY(username) REFERENCES users(username), FOREIGN KEY(qid) REFERENCES quiz(qid));
 
+
 insert into users (username,overall) values ('g',0);
-insert into quiz (qname) values ('celebrity');
+insert into quiz (qname) values ('Celebrity');
 insert into quiz (qname) values ('G.K');
 insert into user_quiz (qid,username,result) values (1,'g',5);
 insert into user_quiz (qid,username,result) values (2,'g',4);
